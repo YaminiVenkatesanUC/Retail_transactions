4cf26e1ea0bcc312434abe2efb0df60c608c4af3
YaminiVenkatesan <yve14@uclive.ac.nz>
2022-06-29 12:08:05 +0530

    Update retail_data_analysis.sql
    Added comments

--- retail_data_analysis.sql
+++ retail_data_analysis.sql
@@ -1,40 +1,46 @@
 
-USE test_DB
+USE test_DB 
+
 -- EDA
 
---select * from dbo.Retail_transactions_data where trans_date is not null
---select count(DISTINCT(customer_id)) from dbo.Retail_transactions_data -- 6889
+--select * from dbo.Retail_transactions_data where trans_date is null or tran_amount is null
+--select count(DISTINCT(customer_id)) from dbo.Retail_transactions_data -- Row count -> 6889
 
---select max(trans_date) from dbo.Retail_transactions_data	-- 14/03
---select max(tran_amount) from dbo.Retail_transactions_data -- 105
+--select max(trans_date) from dbo.Retail_transactions_data	-- Last purschased date in the table -> 2022-03-14
+--select max(tran_amount) from dbo.Retail_transactions_data -- Maximum amount purschased -> 105
 
 -- Query to return the RFM values --
-select customer_id, 
-       DATEDIFF(DAY,max(trans_date),GETDATE()) as recent_trans_date,
-       count(*) as trans_count,
-       avg(tran_amount) as avg_amount
-into temp
-from dbo.Retail_transactions_data
-group by customer_id
-select * from temp
-select customer_id, 
-       ntile(2) over (order by recent_trans_date ASC) as R,
-       ntile(2) over (order by trans_count DESC) as F,
-       ntile(2) over (order by avg_amount DESC) as M
+SELECT customer_id, 
+       DATEDIFF(DAY,MAX(trans_date),GETDATE()) as recent_trans_date,
+       COUNT(*) as trans_count,
+       AVG(tran_amount) as avg_amount
+INTO temp
+FROM dbo.[Retail_transactions_data]
+GROUP BY customer_id
+--SELECT * FROM temp
+-- NTILE window function is used to split the 
+-- customer ids into 2 equal buckets namely high and low
+-- based on the recent transaction date, transaction count
+-- by customer and average amount spent by each customer
+SELECT customer_id, 
+       NTILE(2) OVER (ORDER BY recent_trans_date ASC) AS R,
+       NTILE(2) OVER (ORDER BY trans_count DESC) AS F,
+       NTILE(2) OVER (ORDER BY avg_amount DESC) AS M
 INTO temp2
-from temp
+FROM temp
 
-select customer_id, R*100 + F*10 + M as RFM_number, 
-case when R = 2 and F = 2 and M = 2 THEN 'SUPERFANS'
-	 when R = 2 and F = 2 and M = 1 THEN 'LOYAL'
-	 when R = 2 and F = 1 and M = 2 THEN 'INFREQUENT SHOPPERS'
-	 when R = 2 and F = 1 and M = 1 THEN 'OTHER ACTIVE'
-	 when R = 1 and F = 1 and M = 1 THEN 'DEAD'
-	 else 'OTHERS'
-end as rfm_category
-from temp2
+SELECT customer_id, R*100 + F*10 + M AS rfm_number, 
+CASE WHEN R = 2 and F = 2 and M = 2 THEN 'SUPERFANS'
+	 WHEN R = 2 and F = 2 and M = 1 THEN 'LOYAL'
+	 WHEN R = 2 and F = 1 and M = 2 THEN 'INFREQUENT SHOPPERS'
+	 WHEN R = 2 and F = 1 and M = 1 THEN 'OTHER ACTIVE'
+	 WHEN R = 1 and F = 1 and M = 1 THEN 'DEAD'
+	 ELSE 'OTHERS'
+END AS rfm_category
+FROM temp2 
+ORDER BY customer_id
 
 -- Commands to drop the temporary table
-DROP table temp
-DROP table temp2
+DROP TABLE temp
+DROP TABLE temp2
 
